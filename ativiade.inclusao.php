98a1028a15ae2de3293a8f863d93e62b14253a78
pisanilivia16 <lelilucianoe@gmail.com>
2025-05-05 10:57:39 -0300

    Create ativiade.inclusao.php

---
+++ ativiade.inclusao.php
@@ -0,0 +1,21 @@
+<HTML>
+<HEAD>
+ <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+ <TITLE>Consulta</TITLE>
+</HEAD>
+<BODY>
+<font face="book antiqua" size="8"><center>Consulta de Registros</center></font><br>
+
+<form method="post" action="consultar.php">
+Digite a expressão: <input type="text" name = "expressao"><br><br>
+
+Escolha a opção:
+<input type="radio" name="op" value ="veiculo" checked>Veículo
+<input type="radio" name="op" value ="nome">Nome
+<input type="radio" name="op" value ="marca">Marca
+<input type="radio" name="op" value ="ano">Ano
+
+<input type="submit" value ="Consultar">
+</form>
+</BODY>
+</HTML>
